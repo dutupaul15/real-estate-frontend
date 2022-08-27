f00aa1c66085ca25c8cfe99a040d17d89cbe14b0
Dutu Paul <dutu_paul15@yahoo.com>
2022-08-27 21:39:14 +0300

    first commit, quick set up

---
+++ my-app/src/components/Login/Login.js
@@ -0,0 +1,5 @@
+import React, { useEffect, useState } from 'react';
+import { Button, Grid, Typography } from '@mui/material';
+import { CssBaseline } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
